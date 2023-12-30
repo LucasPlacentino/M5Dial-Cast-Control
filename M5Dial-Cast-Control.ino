75320eae6fcc0889b50f9597d40caf155756dabd
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-12-30 12:50:54 +0100

    Create M5Dial-Cast-Control.ino

---
+++ M5Dial-Cast-Control.ino
@@ -0,0 +1,19 @@
+/**
+  * M5Dial-Cast-Control
+  * Control your Cast/Chromecast devices with an M5Dial
+  *
+  * Uses the ArduCastControl library
+  *
+  * Apache2.0 License
+  * by Lucas Placentino
+  */
+
+
+void setup() {
+    //setup
+}
+
+void loop() {
+    //loop
+}
+
